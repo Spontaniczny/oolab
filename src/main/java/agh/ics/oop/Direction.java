5a293b9b0ec6ca83e68a966deb4607e3d83c3f56
Spontaniczny <k2002kuba@gmail.com>
2022-10-06 00:52:43 +0200

    Completed 1-15

---
+++ src/main/java/agh/ics/oop/Direction.java
@@ -0,0 +1,10 @@
+package agh.ics.oop;
+
+public class Direction {
+    public enum direction {
+        FORWARD,
+        BACKWARD,
+        RIGHT,
+        LEFT
+    }
+}
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,4 +1,34 @@
 package agh.ics.oop;
+//import Direction;
 
 public class World {
+    public static void main(String[] args) {
+        System.out.println("system wystartowal");
+
+//        run(new String[] {"a", "b"});
+//        convert_to_enum();
+        run(args);
+        System.out.println("system zakonczyl dzialanie");
+    }
+    public static void run(String[] tab){
+        for(String arg : tab) {
+            String message = switch (arg) {
+                case "f" -> "Do przodu";
+                case "b" -> "Do tyłu";
+                case "l" -> "W lewo";
+                case "r" -> "W prawo";
+                default -> "Nieznana komenda";
+            };
+            if(!message.equals("Nieznana komenda")) {
+                System.out.println(message);
+            }
+        }
+//        for (int i = 1; i < tab.length; ++i){
+//            System.out.print(", ");
+//            System.out.print(tab[i]);
+//        }
+    }
+    public static void convert_to_enum(String[] tab){
+
+    }
 }
